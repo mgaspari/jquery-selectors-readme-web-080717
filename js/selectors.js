e4f6703ff1c13bbafdacf66db8eb4246fd373963
mgaspari <marcoanthonygaspari@gmail.com>
2017-09-22 19:14:15 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
 // declare your functions here...
+var paragraphSelector = () => {
+  return $('p')
+};
+
+var lastImageSelector = () => {
+  return $('img:last')
+}
+ var ninjaBabySelector = () => {
+   return $("img[id= 'baby-ninja']")
+ }
+
+var divSelector = () => {
+  return $("div[class = 'pics']")
+}
+
+var firstListItem = () => {
+  return $("ul li:first-child")
+}
